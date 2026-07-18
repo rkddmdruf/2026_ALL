90e854d68873f24a77e2e765285568d5b2c8d68e
rkddmdruf <gur81119@gmail.com>
2026-07-18 19:27:46 +0900

    -_-

--- Projects/2_6/src/main/scdule.java
+++ Projects/2_6/src/main/scdule.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +25,8 @@ import static uitls.BoxPanel.*;
 import static uitls.Properties.*;
 
 public class scdule extends CFrame{
+	LocalTime firstTime = LocalTime.of(6, 0);
+	final LocalTime endTime = LocalTime.of(23, 0);
 	LocalDateTime ldt = LocalDateTime.now();
 	List<stationEntity> stations = stationEntity.findAll();
 	List<Integer> totalPix = Arrays.asList(0);
@@ -40,23 +43,26 @@ public class scdule extends CFrame{
 
 	@Override
 	protected void desing() {
+		setTime();
 		JLabel logo = new JLabel(new ImageIcon(Util.logo.getScaledInstance(110, 40, 4)));
 		JLabel seLabel = lb(start.name + " → " + end.name, FONT(getter.font.deriveFont(16f).deriveFont(1)));
 		CButton bt = comp(CButton::new , TEXT("시간 변경"), SIZE(110, 25), FG(Color.white), BG(getter.color));
 		bt.arc = 25;
-		
 		JLabel timeLine = lb("<html>서구청<br>입니다</html>", FONT(getter.font), HOA(JLabel.LEFT), BORDER(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.LIGHT_GRAY)));
 		JPanel topPanel = set(col(10, 0, 0, row(0, logo, hg(60),  seLabel).setBackColor(Color.white), row(5, bt, tLabel).setBackColor(Color.white), fw(timeLine)).setBackColor(Color.white), BORDER(getter.em(5, 5, 0, 5)));
 		JScrollPane sc = set(new JScrollPane(setPanel(), JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BG(Color.white), BORDER(getter.em(5, 5, 5, 5)));
 		add(col(0, fw(topPanel), f(sc)).setBackColor(Color.white));
 	}
-
+	
 	private JPanel setPanel() {
-		JPanel panel = new JPanel(new GridLayout(0, 1, 10, 10));
-		panel.setBorder(getter.em(5, 5, 5, 5));
-		panel.setBackground(Color.white);
+		System.out.println(totalPix.get(0) * 0.05 /40*60);
+		JPanel mainPanel = new JPanel(new GridLayout(0, 1, 10, 10));
+		mainPanel.setBorder(getter.em(5, 5, 5, 5));
+		mainPanel.setBackground(Color.white);
+		int totalTime = (int) Math.ceil((totalPix.get(0) * 0.05 /40*60));
 		for(int i = 0; i < 10; i++) {
-			JPanel p = new JPanel(new BorderLayout()) {
+			
+			JPanel panel = new JPanel(new BorderLayout()) {
 				@Override
 				protected void paintComponent(Graphics g) {
 					super.paintComponent(g);
@@ -66,13 +72,30 @@ public class scdule extends CFrame{
 					g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 20, 20);
 				}
 			};
-			set(p, BORDER(getter.em(5, 45, 5, 45)), BG(Color.white), SIZE(0, 100));
+			set(panel, BORDER(getter.em(5, 45, 5, 45)), BG(Color.white), SIZE(0, 100));
 			
+			JPanel p = new JPanel(new GridLayout(3, 3, 0, 0));
+			p.add(lb(start.name, FONT(getter.font.deriveFont(16f))));
+			p.add(lb("[I2->I7]", HOA(JLabel.CENTER)));
+			p.add(lb(end.name, FONT(getter.font.deriveFont(16f))));
+			p.add(lb(firstTime.format(DateTimeFormatter.ofPattern("hh:mm")),FONT(getter.font.deriveFont(17f).deriveFont(1)), FG(getter.color)));
+			p.add(lb("→", FG(getter.color), HOA(JLabel.CENTER)));
+			p.add(lb(firstTime.plusMinutes(totalTime).format(DateTimeFormatter.ofPattern("hh:mm")),FONT(getter.font.deriveFont(17f).deriveFont(1)), FG(getter.color)));
+			JLabel img = new JLabel(getter.getImage("icon/trains", 50, 25));
+			img.setHorizontalAlignment(JLabel.LEFT);
+			p.add(img);
+			p.add(lb(totalTime + "분 소요", FONT(getter.font.deriveFont(11f)), HOA(JLabel.CENTER)));
+			set(p, BG(Color.white));
 			panel.add(p);
+			mainPanel.add(panel);
+			firstTime = firstTime.plusMinutes(15);
 		}
-		return panel;
+		return mainPanel;
 	}
 	
+	private void setTime() {
+		while(!firstTime.isAfter(ldt.toLocalTime())) firstTime = firstTime.plusMinutes(15);
+	}
 	@Override
 	protected void action() {
 		// TODO Auto-generated method stub
@@ -80,6 +103,6 @@ public class scdule extends CFrame{
 	}
 
 	public static void main(String[] args) {
-		Util.start(new scdule(34, 38));
+		Util.start(new scdule(43, 9));
 	}
 }
